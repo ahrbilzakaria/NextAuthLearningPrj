e81164f2538bd1679f1ee0e03407fdae751f6b86
ahrbilzakaria <zakariaahr@gmail.com>
2024-06-20 09:45:43 +0100

    name update settings

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,5 +1,7 @@
 import { Metadata } from "next";
 import SettingsPage from "./SettingsPage";
+import { auth } from "@/auth";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
   title: "Settings",
@@ -7,6 +9,11 @@ export const metadata: Metadata = {
 
 export default async function Page() {
   // TODO: Protect this page via authentication
-
-  return <SettingsPage />;
+  const session = await auth();
+  const user = session?.user;
+  // if (!user) return <NotFound></NotFound>;
+  if (!user) {
+    redirect("/signin?callbackUrl=/settings");
+  }
+  return <SettingsPage user={user} />;
 }
