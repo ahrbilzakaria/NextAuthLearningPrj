3f9ceee7f451806d1350e2a40a4ed4ca72e424c8
ahrbilzakaria <zakariaahr@gmail.com>
2024-06-21 00:35:15 +0100

    final

--- src/auth.ts
+++ src/auth.ts
@@ -1,12 +1,12 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
-import Google from "next-auth/providers/google";
 import prisma from "./lib/prisma";
 import GoogleProvider from "next-auth/providers/google";
 import GithubProvider from "next-auth/providers/github";
+import { Adapter } from "next-auth/adapters";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(prisma) as Adapter,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
